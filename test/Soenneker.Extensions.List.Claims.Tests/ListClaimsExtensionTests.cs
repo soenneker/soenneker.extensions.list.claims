41d0adc45a3264e28c098132d245ed770220b123
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:59 -0500

    TUnit conversion

--- test/Soenneker.Extensions.List.Claims.Tests/ListClaimsExtensionTests.cs
+++ test/Soenneker.Extensions.List.Claims.Tests/ListClaimsExtensionTests.cs
@@ -1,13 +1,12 @@
 using Soenneker.Tests.Unit;
 using System.Collections.Generic;
 using System.Security.Claims;
-using Xunit;
 
 namespace Soenneker.Extensions.List.Claims.Tests;
 
 public sealed class ListClaimsExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void ToClaimsPrincipal_should_set_IsAuthenticated_true_when_authenticationType_is_provided()
     {
         var claims = new List<Claim> {new(ClaimTypes.NameIdentifier, "123")};
@@ -19,7 +18,7 @@ public sealed class ListClaimsExtensionTests : UnitTest
         Assert.Contains(principal.Claims, c => c.Type == ClaimTypes.NameIdentifier && c.Value == "123");
     }
 
-    [Fact]
+    [Test]
     public void ToClaimsPrincipal_should_set_IsAuthenticated_false_when_authenticationType_is_empty()
     {
         var claims = new List<Claim> {new(ClaimTypes.NameIdentifier, "123")};
@@ -30,7 +29,7 @@ public sealed class ListClaimsExtensionTests : UnitTest
         Assert.False(principal.Identity!.IsAuthenticated);
     }
 
-    [Fact]
+    [Test]
     public void ToClaimsPrincipal_should_return_empty_principal_when_claims_are_empty()
     {
         var claims = new List<Claim>();
